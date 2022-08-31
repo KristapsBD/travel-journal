78d59e9f3ad61c8ade653ed3615c503d91e1fb2a
Kristaps Briks <kristaps.briks@inbox.lv>
2022-08-31 21:59:19 +0300

    Implement single static section

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import { ImLocation } from 'react-icons/im';
 
 const Section = () => {
-  return (
-    <div>Section</div>
-  )
+    return (
+        <section>
+            <img src='https://source.unsplash.com/WLxQvbMyfas' alt='japan' />
+            <div className='text-content'>
+                <div className='head'>
+                    <ImLocation className='location-icon' />
+                    <p className='country'>JAPAN</p>
+                    <p className='view-maps'>View on Google Maps</p>
+                </div>
+                <h1>Mount Fuji</h1>
+                <p className='date'>12 Jan, 2021 - 24 Jan, 2021</p>
+                <p className='description'>
+                    Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet).
+                    Mount Fuji is the single most popular tourist site in Japan,
+                    for both Japanese and foreign tourists.
+                </p>
+            </div>
+        </section>
+    )
 }
 
 export default Section
